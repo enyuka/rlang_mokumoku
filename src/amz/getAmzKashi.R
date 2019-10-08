8f0501f456b6fb708fd6631e8da37de75b55b838
enyuka <nakamura345@gmail.com>
2019-10-08 14:05:51 +0900

    [Mod]意味の分かりづらい変数名を修正

--- src/amz/getAmzKashi.R
+++ src/amz/getAmzKashi.R
@@ -2,20 +2,25 @@ library(rvest)
 library(stringr)
 
 amz_kashi_url = "https://www.uta-net.com/search/?Keyword=amazarashi"
-url_trs =
+trs =
   read_html(amz_kashi_url, encoding = "UTF-8") %>%
   html_nodes("tbody > tr")
-url_df = NULL
+kashi_df = NULL
 
-for (i in 1:length(url_trs)) {
-  tr = url_trs[i]
+for (i in 1:length(trs)) {
+  tr = trs[i]
   songwriter = html_nodes(tr, ".td3") %>% html_text()
   if (!grepl("秋田ひろむ", songwriter)) {
     # 秋田さん以外の曲は飛ばす
     next
   }
+  
+  # 曲名取得
   title_node = html_nodes(tr, ".td1")
   title_text = title_node %>% html_text()
+  
+  # html_node"s"じゃなくて、html_nodeなのは該当のtd配下に王冠アイコンがあって、
+  # 歌詞とは別のリンクが埋め込まれているから、1つ目だけを取得
   song_url = html_node(title_node, "a") %>% html_attr("href")
   target_url = paste("https://www.uta-net.com", song_url, sep = "")
   
@@ -25,21 +30,21 @@ for (i in 1:length(url_trs)) {
   # 歌詞を取得
   kashi_text = html_nodes(kashi_html, "#kashi_area") %>% html_text()
   
-  # 歌の発売日DOMを取得
+  # 曲の発売日を取得し、年、月、日で分割しておく
   hatsubaibi_split =
     html_nodes(kashi_html, "#view_amazon") %>%
     html_text() %>%
     str_extract(pattern = "\\d{4}-\\d{2}-\\d{2}") %>%
     str_split(pattern = "-")
   
-  url_df_i = data.frame(
+  temp_df = data.frame(
     title = title_text,
     kashi = kashi_text,
     hatsubaibi_year = hatsubaibi_split[[1]][1],
     hatsubaibi_month = hatsubaibi_split[[1]][2],
     hatsubaibi_day = hatsubaibi_split[[1]][3]
   )
-  url_df = rbind(url_df, url_df_i)
+  kashi_df = rbind(kashi_df, temp_df)
   
   # 良心
   Sys.sleep(1)
