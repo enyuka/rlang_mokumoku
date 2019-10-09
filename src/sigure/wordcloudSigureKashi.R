d3149e54a5f20e473565da39ab9e428c199d761f
enyuka <nakamura345@gmail.com>
2019-10-09 17:41:48 +0900

    [Add]打首と時雨のWord Cloud取得するソースコード追加

---
+++ src/sigure/wordcloudSigureKashi.R
@@ -0,0 +1,24 @@
+# devtools::install_github("bmschmidt/wordVectors")
+library(RMeCab)
+library(wordcloud)
+library(dplyr)
+
+# 文字化け対策
+par(family = "HiraKakuProN-W3")
+
+# 分かち書き
+rmecab_result = docDF(sigure_kashi_df, col = 2, type = 1)
+# 数や非自立などの単語を除外し、data.frameに代入
+wordcloud_source = rmecab_result %>% filter(POS1 %in% c("名詞"), POS2 != "数", POS2 != "サ変接続", POS2 != "非自立", TERM != ")", TERM != "(")
+
+# kashi_freqは出現数の列を切り出し、sumしたdata.frame
+kashi_freq = apply(wordcloud_source[, 4:ncol(wordcloud_source)], 1, sum)
+wordcloud_source = data.frame(wordcloud_source, kashi_freq)
+wordcloud(
+  wordcloud_source$TERM,
+  wordcloud_source$kashi_freq,
+  min.freq = 5,
+  random.order = FALSE,
+  scale = c(6, .5),
+  colors = brewer.pal(8, "Dark2")
+)
---
+++ src/uchikubi/getUchikubiKashi.R
@@ -0,0 +1,45 @@
+library(rvest)
+library(stringr)
+
+amz_kashi_url = "https://www.uta-net.com/search/?Keyword=打首獄門同好会"
+trs =
+  read_html(amz_kashi_url, encoding = "UTF-8") %>%
+  html_nodes("tbody > tr")
+uchikubi_kashi_df = NULL
+
+for (i in 1:length(trs)) {
+  tr = trs[i]
+  # 曲名取得
+  title_node = html_nodes(tr, ".td1")
+  title_text = title_node %>% html_text()
+  
+  # html_node"s"じゃなくて、html_nodeなのは該当のtd配下に王冠アイコンがあって、
+  # 歌詞とは別のリンクが埋め込まれているから、1つ目だけを取得
+  song_url = html_node(title_node, "a") %>% html_attr("href")
+  target_url = paste("https://www.uta-net.com", song_url, sep = "")
+  
+  # 歌詞のページ
+  kashi_html = read_html(target_url)
+  
+  # 歌詞を取得
+  kashi_text = html_nodes(kashi_html, "#kashi_area") %>% html_text()
+  
+  # 曲の発売日を取得し、年、月、日で分割しておく
+  hatsubaibi_split =
+    html_nodes(kashi_html, "#view_amazon") %>%
+    html_text() %>%
+    str_extract(pattern = "\\d{4}-\\d{2}-\\d{2}") %>%
+    str_split(pattern = "-")
+  
+  temp_df = data.frame(
+    title = title_text,
+    kashi = kashi_text,
+    hatsubaibi_year = hatsubaibi_split[[1]][1],
+    hatsubaibi_month = hatsubaibi_split[[1]][2],
+    hatsubaibi_day = hatsubaibi_split[[1]][3]
+  )
+  uchikubi_kashi_df = rbind(uchikubi_kashi_df, temp_df)
+  
+  # 良心
+  Sys.sleep(1)
+}
